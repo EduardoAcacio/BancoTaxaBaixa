7e2f9b7ec43476ec3adc9a815d7e85d4a1cead32
Eduardo Acácio <acaciosh@gmail.com>
2017-03-10 15:43:12 -0300

    Add files via upload
    Novas edições

---
+++ Conections.java
@@ -0,0 +1,21 @@
+package com.taxabaixa.DB;
+
+import java.sql.*;
+
+public class Conections
+{
+	public static void main( String args[] )
+	{
+		Connection c = null;
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			c.setAutoCommit(false);
+			System.out.println("Opened database successfully\n");
+		} catch ( Exception e ) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			System.exit(0);
+		}
+	}
+}
--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.taxabaixa;
 public class Main {
 
 	public static void main(String[] args) {
-		Agencia ag = new Agencia(2,1);
+		//Agencia ag = new Agencia(2,1);
 
 	}
 
